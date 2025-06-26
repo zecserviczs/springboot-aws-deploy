1b3c1976f1deedc4392f3e8dbc1de6504e6b4bcf
eddy zam <eddyzam@eddys-MacBook-Air.local>
2025-06-26 05:25:54 +0200

    first commit

---
+++ src/test/java/com/zecservices/SpringbootAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zecservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
